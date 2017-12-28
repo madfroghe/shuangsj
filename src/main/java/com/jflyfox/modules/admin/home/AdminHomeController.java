4904d436ce77229a847cd55d64ed1fb2c890f6d1
hechao_mi <hechao_mi@great-tao.com>
2017-12-28 09:12:47 +0800

    主体界面更新

--- src/main/java/com/jflyfox/modules/admin/home/AdminHomeController.java
+++ src/main/java/com/jflyfox/modules/admin/home/AdminHomeController.java
@@ -22,33 +22,6 @@ public class AdminHomeController extends BaseProjectController {
 			return;
 		}
 		setAttr("nowUser", user);
-
-		
-		// 最新文件
-		Page<TbArticle> articlePage = TbArticle.dao.paginate(new Paginator(1, 10), "select t.*,f.name as folderName " //
-				, " from tb_article t left join tb_folder f on f.id = t.folder_id " //
-						+ " where t.status = 1 and t.type in (11,12) " // 查询状态为显示，类型是预览和正常的文章
-						+ "  and f.site_id="+getBackSite().getId()
-						+ " order by t.update_time desc,t.id desc");
-		setAttr("articles", articlePage.getList());
-
-		// 最新评论
-		Page<TbComment> commentPage = TbComment.dao.paginate(new Paginator(1, 10), "select t.*,a.title articleName ", //
-				" from tb_comment t " //
-						+ " left join tb_article a on a.id = t.article_id where 1=1 order by t.id desc  ");
-		setAttr("comments", commentPage.getList());
-
-		// 最新用户
-		Page<SysUser> userPage = SysUser.dao.paginate(new Paginator(1, 10), "select t.*,d.name as departname ", //
-				" from sys_user t left join sys_department d on d.id = t.departid " //
-						+ " where 1 = 1 and userid != 1 order by userid desc ");
-		setAttr("users", userPage.getList());
-
-		
-		// 最新访问用户
-		Page<TbPageView> pageViewPage = TbPageView.dao.paginate(new Paginator(1, 10), "select t.*", //
-				" from tb_pageview t order by id desc ");
-		setAttr("pageViews", pageViewPage.getList());
 		
 		
 		render(path + "home.html");
